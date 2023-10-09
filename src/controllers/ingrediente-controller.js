60d92a935075e624ef5dbb7d54d4c45b061ffd0e
feliepmarinscardoso <felipemarinscardoso@gmail.com>
2023-10-09 11:18:21 -0300

    fix: compra ingrediente

--- src/controllers/ingrediente-controller.js
+++ src/controllers/ingrediente-controller.js
@@ -236,7 +236,7 @@ module.exports = {
             }
 
             const novo_estoque = ingrediente[0].estoque_atual + quantidade_liquida;
-            const [result] = await mysql.execute(query, [id_ingrediente, data_compra, quantidade_bruta, pre_limpeza, quantidade_liquida, valor_unitario, valor_total, numero_nota, fornecedor]);
+            const [result] = await mysql.execute(query, [data_compra, id_ingrediente, quantidade_bruta, pre_limpeza, quantidade_liquida, valor_unitario, valor_total, numero_nota, fornecedor]);
             await mysql.execute('INSERT INTO registros (data_registro, id_usuario, id_acao, descricao) VALUES (NOW(), ?, ?, ?)', [decodedToken.id, 4, `O usuário ${decodedToken.nome} comprou ${quantidade_bruta}kg do ingrediente ${id_ingrediente}`]);
             await mysql.execute('UPDATE ingredientes SET estoque_atual = ? WHERE id = ?', [novo_estoque, id_ingrediente]);
             return response.status(201).json({ message: 'Compra de ingrediente realizada com sucesso!', id: result.insertId });
